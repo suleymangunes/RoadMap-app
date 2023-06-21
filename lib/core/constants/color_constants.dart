45ae7b61ace0a76b19fe7cfed639925a573b12f7
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-06-21 20:51:41 +0300

    App icon added and app name edited

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class ColorConstants {
+  static final ColorConstants _colorConstants = ColorConstants._();
+
+  factory ColorConstants() {
+    return _colorConstants;
+  }
+
+  ColorConstants._();
+
+  // #867466
+  static const Color sandDune = Color.fromRGBO(134, 116, 102, 1);
+  // #B4ADA3
+  static const Color nomad = Color.fromRGBO(180, 173, 163, 1);
+}
